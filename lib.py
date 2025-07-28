9673d14f21014be6e76d6bfbcd946199f8ce5306
shivam7659 <150763381+shivam7659@users.noreply.github.com>
2025-07-28 14:42:15 +0530

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,8 +2,8 @@ import os
 with open('usecase.conf', 'r') as file:
     content = file.read()
     print(content)
-os.system('git fetch origin')
-os.system('git checkout origin/main')
+os.system('git fetch origin main:main')
+os.system('git checkout main')
 with open('usecase.conf', 'r') as file:
     content = file.read()
     print(content)
